3b26a3c5a193cf3b52a63f50df7660511ec60da4
deparr <dparrott@protonmail.com>
2025-03-03 12:19:22 -0700

    feat: migrate old config keys to the new ones

--- lua/tairiki/config.lua
+++ lua/tairiki/config.lua
@@ -56,13 +56,67 @@ M.defaults = {
 ---@type tairiki.Config
 M.options = nil
 
----@param opts? tairiki.Config
-function M.setup(opts)
-  ---@diagnostic disable-next-line:undefined-field
-  if opts and opts.ending_tildes then
-    opts.end_of_buffer = opts.ending_tildes ---@diagnostic disable-line:undefined-field
+---@param old_config? table
+--- migrates an old config to a tairiki.Config
+function M.migrate_config(old_config)
+  if not old_config then
+    return
   end
 
+  if type(old_config.style) == "string" then
+    old_config.palette = old_config.style
+    old_config.style = nil
+  end
+
+  if type(old_config.term_colors) == "boolean" then
+    old_config.terminal = old_config.term_colors
+    old_config.term_colors = nil
+  end
+
+  if type(old_config.ending_tildes) == "boolean" then
+    old_config.end_of_buffer = old_config.ending_tildes
+    old_config.ending_tildes = nil
+  end
+
+  if type(old_config.code_style) == "table" then
+    for k, s in pairs(old_config.code_style) do
+      if type(s) == "string" then
+        local st = {}
+        if s ~= "none" then
+          for _, sk in ipairs(vim.split(s, "%s*,%s*")) do
+            st[sk] = true
+          end
+        end
+        old_config.code_style[k] = st
+      end
+    end
+  end
+
+  old_config.toggle_style_key = nil
+  old_config.toggle_style_list = nil
+
+  local found = false
+  if type(old_config.colors) == "table" then
+    old_config.colors = nil
+    found = true
+  end
+  if type(old_config.highlights) == "table" then
+    old_config.highlights = nil
+    found = true
+  end
+  if found then
+    vim.schedule(function()
+      vim.notify(
+        "tairiki: found old-style color or highlight overrides, see default config on how to migrate",
+        vim.log.levels.WARN
+      )
+    end)
+  end
+end
+
+---@param opts? tairiki.Config
+function M.setup(opts)
+  M.migrate_config(opts)
   M.options = vim.tbl_deep_extend("force", {}, M.defaults, opts or {})
 end
 
